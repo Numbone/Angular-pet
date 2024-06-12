e905d05ea284c9e6eb50918867ea2fbf36e73244
Nurlybek <nurlybekqwerty@gmail.com>
2024-06-13 01:24:34 +0500

    add service and pipe,destructer component

--- src/app/common-ui/profile-card/profile-card.component.ts
+++ src/app/common-ui/profile-card/profile-card.component.ts
@@ -1,15 +1,18 @@
-import { Component } from '@angular/core';
+import {Component, Input} from '@angular/core';
 import {NgOptimizedImage} from "@angular/common";
+import {Profile} from "../../data/interfaces/profile.interface";
+import {ImgUrlPipe} from "../../helpes/pipes/img-url.pipe";
 
 @Component({
   selector: 'app-profile-card',
   standalone: true,
   imports: [
-    NgOptimizedImage
+    NgOptimizedImage,
+    ImgUrlPipe
   ],
   templateUrl: './profile-card.component.html',
   styleUrl: './profile-card.component.scss'
 })
 export class ProfileCardComponent {
-
+  @Input() profile!:Profile;
 }
