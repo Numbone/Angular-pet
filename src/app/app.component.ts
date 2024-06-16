fa43662ec1e9e245c9d82a60645b1d755141e34f
Nurlybek <nurlybekqwerty@gmail.com>
2024-06-17 00:43:20 +0500

    add login page and form component

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,22 +1,16 @@
-import {Component, inject} from '@angular/core';
+import {Component} from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import {ProfileCardComponent} from "./common-ui/profile-card/profile-card.component";
-import {ProfileService} from "./data/services/profile.service";
-import {JsonPipe} from "@angular/common";
-import {Profile} from "./data/interfaces/profile.interface";
+
+
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet, ProfileCardComponent, JsonPipe,],
+  imports: [RouterOutlet, ProfileCardComponent,],
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss'
 })
 export class AppComponent {
-  title = 'tic-talk';
-  profileService=inject(ProfileService);
-  profiles:Profile[]=[]
-  constructor(){
-    this.profileService.getTestAccounts().subscribe(value=>this.profiles=value)
-  }
+
 }
--- src/app/pages/login-page/login-page.component.ts
+++ src/app/pages/login-page/login-page.component.ts
@@ -1,12 +1,21 @@
 import { Component } from '@angular/core';
+import {FormControl, FormGroup, ReactiveFormsModule, Validators} from "@angular/forms";
 
 @Component({
   selector: 'app-login-page',
   standalone: true,
-  imports: [],
+  imports: [
+    ReactiveFormsModule
+  ],
   templateUrl: './login-page.component.html',
   styleUrl: './login-page.component.scss'
 })
 export class LoginPageComponent {
-
+  public form = new FormGroup({
+    username:new FormControl(null, [Validators.required]),
+    password: new FormControl(null, [Validators.required])
+  })
+  onSubmit(){
+    console.log(this.form.value)
+  }
 }
